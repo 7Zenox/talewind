68ccaa85c8dfee2e15d91e76858689586566b7ac
vasu7400 <vasu7400@me.com>
2023-05-02 23:47:16 +0530

    front end update
    - added spacy render
    - fixed cursor bug
    - fixed pxlbg bug

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { NextUIProvider, createTheme } from '@nextui-org/react'
+import { rgba } from '@react-spring/shared';
 import { ThemeProvider as NextThemesProvider, useTheme as useNextTheme } from 'next-themes';
 import dynamic from 'next/dynamic'
 
@@ -11,14 +12,11 @@ const darkTheme = createTheme({ type: "dark", theme: "dark" })
 const lightTheme = createTheme({ type: "light", theme: "light" })
 
 function MyApp({ Component, pageProps }) {
-  const { resolvedTheme } = useNextTheme();
-  // if the theme is dark, set the color to white, else set it to black
-  const color = "255, 255, 255";
-  
   return (
     <>
       <AnimatedCursor
-        color={`${color}`}
+        color={"0, 150, 255"}
+        opacity={0.5}
         innerSize={20}
         outerSize={35}
         innerScale={1}
@@ -26,7 +24,7 @@ function MyApp({ Component, pageProps }) {
         outerAlpha={0}
         trailingSpeed={7}
         outerStyle={{
-          border: `1px solid rgba(${color}, 0.8)`
+          border: `1px solid rgb(0, 150, 255)`
         }} />
 
       <NextThemesProvider
