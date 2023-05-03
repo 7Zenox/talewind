d694cba5fa104906d3aae8f94b6a628bfe6049e1
hauntedcupoftea <andydchauhan@gmail.com>
2023-05-03 13:34:59 +0530

    added basic FastAPI app (probably doesn't work lol)

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI
-from keras.models import load_model
+from fastapi import FastAPI, Request
+from ..AI.TalewindPredictor import TalewindPredictor
 
 app = FastAPI()
+predictor = TalewindPredictor(path_to_model="path/to/your/model")
 
+@app.post("/predict")
+async def predict(request: Request):
+    data = await request.json()
+    query = data["query"]
+    mode = data["mode"]
+    result = predictor(query, mode)
+    return result
