364b5928800e92baa0bbbb78b33fa7ca0eeefea1
hauntedcupoftea <andydchauhan@gmail.com>
2023-05-03 15:24:08 +0530

    finish basic backend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI, Request
-from ..AI.TalewindPredictor import TalewindPredictor
+from pydantic import BaseModel
+from TalewindPredictor import TalewindPredictor
 
 app = FastAPI()
-predictor = TalewindPredictor(path_to_model="../AI/models/Dense_NN_CURRENT-BEST_20230502-190500.h5")
+predictor = TalewindPredictor(path_to_model="../AI/models/Dense_NN_CURRENT-BEST_20230502-190500.h5",
+                              color_path='../AI/colors.pkl')
+
+class JSONObject(BaseModel):
+    query: str
+    mode: str
+
+@app.get('/')
+async def hello():
+    return {"message" : "this is the backend. why are you here?"}
 
 @app.post("/predict")
-async def predict(request: Request):
-    data = await request.json()
-    query = data["query"]
-    mode = data["mode"]
+async def predict(request: JSONObject):
+    query = request.query
+    mode = request.mode
     result = predictor(query, mode)
     return result
